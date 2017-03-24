fd0d5a8f83490ca821fb93863535cd9d722cf335
vincent-noel <vincent.noel@butantan.gov.br>
2017-03-24 18:03:41 -0300

    Added a script to fix the bug in addChild in libsedml

---
+++ fix_libsedml_addChild.py
@@ -0,0 +1,36 @@
+import libsedml, shutil, os
+
+# Getting the filenames
+original_file = libsedml.__file__[:-1]
+original_filepath = os.path.dirname(original_file)
+backup_file = os.path.join(original_filepath, "__init__.backup")
+
+# Making a backup
+shutil.copy(original_file, backup_file)
+
+# Deleting the original
+os.remove(original_file)
+
+original = open(os.path.join(original_filepath, "__init__.backup"), "r")
+# Reading the backup
+lines = []
+for line in original:
+	lines.append(line)
+
+original.close()
+
+# Writing the new file
+new = open(os.path.join(original_filepath, "__init__.py"), "w")
+for i, line in enumerate(lines):
+	
+	if (		i < len(lines)-4
+		and
+			line.startswith("        if args[0] is not None: args[0].thisown = 0")
+		and
+			lines[i+3].startswith("        return _libsedml.ASTNode_addChild(self, disownedChild, inRead)")
+	):
+		new.write("        if disownedChild is not None: disownedChild.thisown = 0\n")
+	else:
+		new.write(line)
+
+new.close()
