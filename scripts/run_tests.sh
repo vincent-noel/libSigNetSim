d01f55f6c79df78f0ebe6969cab4424a514d7de5
vincent-noel <vincent.noel@butantan.gov.br>
2017-04-02 08:13:14 -0300

    Adding a third test environment for biomodels

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,7 @@
 if [ $1 = "sbml-test-suite" ]; then
     python -m unittest libsignetsim.tests.sbmltestsuite.TestSuite
+elif [ $1 = "biomodels" ]; then
+    python -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
 else
     python -m unittest libsignetsim.simulation.tests.TestTimeseries
     python -m unittest libsignetsim.simulation.tests.TestSteadyStates
@@ -7,5 +9,4 @@ else
     python -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
     python -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
     python -m unittest libsignetsim.combine.tests.TestRunSedmls
-    python -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
 fi
