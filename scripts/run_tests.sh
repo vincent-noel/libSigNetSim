0bc474762ecb756ec92c75b8974072c7a9620004
vincent-noel <vincent.noel@butantan.gov.br>
2017-04-02 09:45:02 -0300

    Adding the exclusion of travis virtualenv for test coverage

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,12 +1,12 @@
 if [ $1 = "sbml-test-suite" ]; then
-    coverage run --omit=*/venv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite
 elif [ $1 = "biomodels" ]; then
-    coverage run --omit=*/venv/* -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
 else
-    coverage run --omit=*/venv/* -m unittest libsignetsim.simulation.tests.TestTimeseries
-    coverage run --omit=*/venv/* -m unittest libsignetsim.simulation.tests.TestSteadyStates
-    coverage run --omit=*/venv/* -m unittest libsignetsim.optimization.tests.TestOptimization
-    coverage run --omit=*/venv/* -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
-    coverage run --omit=*/venv/* -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
-    coverage run --omit=*/venv/* -m unittest libsignetsim.combine.tests.TestRunSedmls
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestTimeseries
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestSteadyStates
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.optimization.tests.TestOptimization
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
+    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.combine.tests.TestRunSedmls
 fi
