b763a74ff7feb0956bdef6c3e59b217b397883cf
vincent-noel <vincent.noel@butantan.gov.br>
2017-04-02 13:16:36 -0300

    Added the append option to coverage, properly this time

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,12 +1,12 @@
 if [ $1 = "sbml-test-suite" ]; then
-    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite
 elif [ $1 = "biomodels" ]; then
-    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
 else
-    coverage run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestTimeseries
-    coverage -a run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestSteadyStates
-    coverage -a run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.optimization.tests.TestOptimization
-    coverage -a run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
-    coverage -a run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
-    coverage -a run --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.combine.tests.TestRunSedmls
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestTimeseries
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestSteadyStates
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.optimization.tests.TestOptimization
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
+    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.combine.tests.TestRunSedmls
 fi
