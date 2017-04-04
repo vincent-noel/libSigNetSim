b7e08483cb749f0f8942d93e916c1942adcdc512
vincent-noel <vincent.noel@butantan.gov.br>
2017-04-04 19:18:01 -0300

    Testing docker integration 14

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,30 +1,32 @@
+OMIT=--omit=*/venv/*,*/virtualenv/*,*/dist-packages/*
+
 if [ $1 = "sbml-test-suite" ]; then
     if [ $2 = "1.2" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l1v2
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l1v2
     elif [ $2 = "2.1" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v1
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v1
     elif [ $2 = "2.2" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v2
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v2
     elif [ $2 = "2.3" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v3
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v3
     elif [ $2 = "2.4" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v4
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v4
     elif [ $2 = "2.5" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v5
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l2v5
     elif [ $2 = "3.1" ]; then
-        coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l3v1
+        coverage run -a $OMIT -m unittest libsignetsim.tests.sbmltestsuite.TestSuite_l3v1
     fi
 elif [ $1 = "biomodels" ]; then
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
+    coverage run -a $OMIT -m unittest libsignetsim.tests.biomodels.TestBiomodelsCompatibility
 else
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.model.tests.TestFindKineticLaws
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.model.tests.TestReduceModel
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestTimeseries
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.simulation.tests.TestSteadyStates
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.optimization.tests.TestOptimization
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.sedml.tests.TestLogscaleTimeseries
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.combine.tests.TestRunSedmls
-    coverage run -a --omit=*/venv/*,*/virtualenv/* -m unittest libsignetsim.combine.tests.TestShowCase
+    coverage run -a $OMIT -m unittest libsignetsim.model.tests.TestFindKineticLaws
+    coverage run -a $OMIT -m unittest libsignetsim.model.tests.TestReduceModel
+    coverage run -a $OMIT -m unittest libsignetsim.simulation.tests.TestTimeseries
+    coverage run -a $OMIT -m unittest libsignetsim.simulation.tests.TestSteadyStates
+    coverage run -a $OMIT -m unittest libsignetsim.optimization.tests.TestOptimization
+    coverage run -a $OMIT -m unittest libsignetsim.sedml.tests.TestBiomodelsURI
+    coverage run -a $OMIT -m unittest libsignetsim.sedml.tests.TestSteadyStatesScan
+    coverage run -a $OMIT -m unittest libsignetsim.sedml.tests.TestLogscaleTimeseries
+    coverage run -a $OMIT -m unittest libsignetsim.combine.tests.TestRunSedmls
+    coverage run -a $OMIT -m unittest libsignetsim.combine.tests.TestShowCase
 fi
