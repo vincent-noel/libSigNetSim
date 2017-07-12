1f610889a9b773d6d59052db9307a9caedc3c779
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-12 11:56:09 -0300

    Trying manual install for libsbml, libnuml and libsbml, with sedml fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,49 @@ EXEC_DIR=$PWD
 DIR=`dirname $PWD/$0`
 INSTALL_DIR=`dirname $DIR`
 
+
+
+$DIR/install_dep.sh
+
+CD /root
+
+svn checkout http://svn.code.sf.net/p/sbml/code/trunk/libsbml/ libsbml
+cd libsbml
+
+mkdir build
+cd build
+
+cmake -DENABLE_COMP=ON -DENABLE_FBC=ON -DENABLE_LAYOUT=ON -DENABLE_QUAL=ON -DENABLE_GROUPS=ON -DENABLE_MULTI=ON -DWITH_EXAMPLES=ON -DWITH_PYTHON=ON ..
+make
+make install
+
+cd ../..
+
+git clone https://github.com/NuML/NuML.git
+cd NuML/libnuml
+
+mkdir build
+cd build
+
+cmake -DEXTRA_LIBS="xml2;z;bz2;" -DLIBSBML_STATIC=ON -DWITH_EXAMPLES=ON -DWITH_PYTHON=ON ..
+make
+make install
+
+cd ../../..
+
+git clone https://github.com/vincent-noel/libSEDML.git
+cd libSEDML
+
+mkdir build
+cd build
+
+cmake -DEXTRA_LIBS="xml2;z;bz2;" -DLIBSBML_STATIC=ON -DLIBNUML_STATIC=ON -DWITH_EXAMPLES=ON -DWITH_PYTHON=ON ..
+make
+make install
+
+cd ../..
+
+ln -s /usr/local/lib/python2.7/site-packages/{libsbml*,libsedml*,libnuml*} /usr/local/lib/python2.7/dist-packages/
+
 python $INSTALL_DIR/setup.py install
-python $INSTALL_DIR/fix_libsedml_addChild.py;
+#python $INSTALL_DIR/fix_libsedml_addChild.py;
--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -2,7 +2,11 @@ apt-get install -y libopenmpi-dev openmpi-bin \
                     libsundials-serial-dev libsundials-serial \
                     liblapack-dev libblas-dev libatlas-dev libatlas-base-dev \
                     python-dev python-pip \
-                    git g++ gcc make unzip wget
+                    git g++ gcc make unzip wget \
+                    subversion \
+                    cmake swig \
+                    zlib1g-dev libxml2-dev libbz2-dev \
+                    libcurl4-openssl-dev
 
 pip install pip --upgrade
 if [ ! -f /usr/bin/pip ]
@@ -10,3 +14,4 @@ then
     ln -s /usr/local/bin/pip /usr/bin/pip
 fi
 easy_install -U distribute
+
