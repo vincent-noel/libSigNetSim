b97bbbe9369086a26da144f3a2bbac11e43f8a94
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-13 16:09:37 -0300

    restoring dependencies needed to install python-libsedml

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -3,7 +3,11 @@ apt-get install -y libopenmpi-dev openmpi-bin \
                     libsundials-serial-dev libsundials-serial \
                     liblapack-dev libblas-dev libatlas-dev libatlas-base-dev \
                     python-dev python-pip \
-                    git g++ gcc make unzip wget 
+                    git g++ gcc make unzip wget \
+                    subversion cmake swig \
+                    zlib1g-dev libxml2-dev libbz2-dev \
+                    libcurl4-openssl-dev \
+                    libxslt1-dev
 
 pip install pip --upgrade
 if [ ! -f /usr/bin/pip ]
