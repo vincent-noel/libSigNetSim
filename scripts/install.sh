888f197f4fdf27705288c10460f78231f6210ef3
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-14 17:08:46 -0300

    Adding fedora dependencies installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@ EXEC_DIR=$PWD
 DIR=`dirname $PWD/$0`
 INSTALL_DIR=`dirname $DIR`
 
-
 pip install -r $INSTALL_DIR/requirements.txt
 
 python $INSTALL_DIR/setup.py install
---
+++ scripts/install_dep_fedora.sh
@@ -0,0 +1,17 @@
+dnf -y update
+dnf -y install openmpi-devel openmpi \
+                sundials sundials-devel \
+                lapack-devel blas-devel atlas-devel atlas-static \
+                python2-pip python2-devel \
+                git gcc gcc-c++ make unzip wget curl
+
+pip install pip --upgrade
+if [ ! -f /usr/bin/pip ]
+then
+    ln -s /usr/local/bin/pip /usr/bin/pip
+fi
+
+easy_install -U distribute
+
+# Installing dependencies for python-libsedml
+curl -sL https://raw.githubusercontent.com/vincent-noel/python-libsedml/master/scripts/install_dep_fedora.sh | bash -
