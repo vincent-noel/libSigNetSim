2215ceec3cda1ab85bcf6768962357b62a94684b
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-14 12:23:28 -0300

    New install_dep script : removed dependencies for python-libsedml, now download python-libsedml install_dep script and run it. Only update pip if version < 9.0.1

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -3,16 +3,23 @@ apt-get install -y libopenmpi-dev openmpi-bin \
                     libsundials-serial-dev libsundials-serial \
                     liblapack-dev libblas-dev libatlas-dev libatlas-base-dev \
                     python-dev python-pip \
-                    git g++ gcc make unzip wget \
-                    subversion cmake swig \
-                    zlib1g-dev libxml2-dev libbz2-dev \
-                    libcurl4-openssl-dev \
-                    libxslt1-dev
+                    git g++ gcc make unzip wget curl
 
-pip install pip --upgrade
-if [ ! -f /usr/bin/pip ]
-then
-    ln -s /usr/local/bin/pip /usr/bin/pip
+
+
+# Installing pip update if pip version < 9.0.1
+currentver="$(pip --version | cut -d" " -f2)"
+requiredver="9.0.1"
+if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" == "$requiredver" ]; then
+    pip install pip --upgrade
+    if [ ! -f /usr/bin/pip ]
+    then
+        ln -s /usr/local/bin/pip /usr/bin/pip
+    fi
 fi
+
+
 easy_install -U distribute
 
+# Installing dependencies for python-libsedml
+curl -sL https://raw.githubusercontent.com/vincent-noel/python-libsedml/develop/scripts/install_dep.sh | bash -
