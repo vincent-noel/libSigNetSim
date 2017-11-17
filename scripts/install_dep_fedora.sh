1935cba48d54b7e76f61b5d675501c9a5fcd27f2
vincent <vincent.noel@butantan.gov.br>
2017-11-17 14:37:23 -0200

    Fixed run docker, fixing fedora deps

--- scripts/install_dep_fedora.sh
+++ scripts/install_dep_fedora.sh
@@ -3,7 +3,6 @@ dnf -y install openmpi-devel openmpi \
                 sundials sundials-devel \
                 lapack-devel blas-devel atlas-devel atlas-static \
                 python2-pip python2-devel redhat-rpm-config \
-                gcc gcc-c++ make
 
 pip install pip --upgrade
 if [ ! -f /usr/bin/pip ]
