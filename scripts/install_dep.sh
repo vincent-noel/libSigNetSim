924d2719ed5d2b92010fdd232cd695a1bb46089c
vincent <vincent.noel@butantan.gov.br>
2018-04-09 19:58:09 -0300

    Added swig dependency for PyDSTool

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -2,7 +2,7 @@ apt-get update -qq
 apt-get install -y libopenmpi-dev openmpi-bin \
                     libsundials-serial-dev libsundials-serial \
                     liblapack-dev libblas-dev libatlas-dev libatlas-base-dev \
-                    python-pip python-dev make
+                    python-pip python-dev make swig
 
 pip install pip --upgrade
 if [ ! -f /usr/bin/pip ]
