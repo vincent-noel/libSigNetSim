019d37b67e0c99d27b02330740f516e4b7541705
vincent <vincent.noel@butantan.gov.br>
2018-05-28 14:40:33 -0300

    Updated dockers

--- scripts/install3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-EXEC_DIR=$PWD
-DIR=`dirname $PWD/$0`
-INSTALL_DIR=`dirname $DIR`
-
-pip3 install -r $INSTALL_DIR/requirements.txt
-
-pip3 install $INSTALL_DIR
--- travis_run.sh
+++ travis_run.sh
@@ -62,13 +62,13 @@ else
                 docker run -di --name test_env -v $(pwd):/home/travis/build/vincent-noel/libSigNetSim signetsim/travis_testenv:stretch-python3 bash || exit 1;
                 docker exec test_env chown -R www-data:www-data /home/travis/ || exit 1;
                 docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim; bash scripts/install_dep.sh 3" || exit 1;
-                docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim; scripts/install3.sh" || exit 1;
+                docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim; scripts/install.sh 2" || exit 1;
 
             else
                 docker run -di --name test_env -v $(pwd):/home/travis/build/vincent-noel/libSigNetSim signetsim/travis_testenv:stretch bash || exit 1;
                 docker exec test_env chown -R www-data:www-data /home/travis/ || exit 1;
                 docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim; bash scripts/install_dep.sh 2" || exit 1;
-                docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim; scripts/install.sh" || exit 1;
+                docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim; scripts/install.sh 2" || exit 1;
             fi
 
             docker exec test_env /bin/bash -c "cd /home/travis/build/vincent-noel/libSigNetSim/libsignetsim/lib/integrate/; make"
