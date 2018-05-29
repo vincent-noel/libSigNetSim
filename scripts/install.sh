b432490562ff0f70eca8b82d6664e556dbd98ffe
vincent <vincent.noel@butantan.gov.br>
2018-05-29 18:28:42 -0300

    Put the gevent dependency in requirements.txt

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,6 @@ else
     PIP_EXECUTABLE="pip3"
 fi
 
-# Bug with gevent 1.3.2, so we prevent it by pre-installing 1.3.1
-${PIP_EXECUTABLE} install gevent==1.3.1
-
 ${PIP_EXECUTABLE} install -r $INSTALL_DIR/requirements.txt
 
 ${PIP_EXECUTABLE} install $INSTALL_DIR
---
+++ scripts/install_dep-fedora.sh
@@ -0,0 +1,81 @@
+if [ -z "$1" ]; then
+    PYTHON_VERSION=3
+else
+    PYTHON_VERSION=$1
+fi
+
+if [[ "${PYTHON_VERSION}" == 2 ]]; then
+    PYTHON_PACKAGES="python2-pip python2-devel"
+    PIP_EXECUTABLE="pip2"
+
+else
+    PYTHON_PACKAGES="python3-pip python3-devel"
+    PIP_EXECUTABLE="pip3"
+fi
+
+dnf -y update
+dnf -y install openmpi-devel openmpi \
+                sundials sundials-devel \
+                lapack-devel blas-devel atlas-devel atlas-static \
+                ${PYTHON_PACKAGES} redhat-rpm-config \
+                make
+
+${PIP_EXECUTABLE} install -i https://pypi.python.org/simple pip --upgrade  --ignore-installed
+
+if [ -f /usr/bin/pip ]
+then
+    mv /usr/bin/pip /usr/bin/pip.bak
+fi
+ln -s /usr/local/bin/pip /usr/bin/pip
+
+if [ -f /usr/bin/${PIP_EXECUTABLE} ]
+then
+    mv /usr/bin/${PIP_EXECUTABLE} /usr/bin/${PIP_EXECUTABLE}.bak
+fi
+ln -s /usr/local/bin/${PIP_EXECUTABLE} /usr/bin/${PIP_EXECUTABLE}
+
+${PIP_EXECUTABLE} install setuptools --upgrade --ignore-installed
+if [[ "${PYTHON_VERSION}" == 3 ]]; then
+    EASY_INSTALL=$(find /usr -name easy_install-3*)
+
+    if [[ -z "${EASY_INSTALL}" ]]; then
+        EASY_INSTALL="easy_install"
+    fi
+
+elif [[ "${PYTHON_VERSION}" == 2 ]]; then
+    EASY_INSTALL=$(find /usr -name easy_install-2*)
+
+    if [[ -z "${EASY_INSTALL}" ]]; then
+        EASY_INSTALL="easy_install"
+    fi
+
+else
+    EASY_INSTALL="easy_install"
+
+fi
+${EASY_INSTALL} --upgrade distribute
+
+# Version incompatibility issue... Hopefully temporary
+${PIP_EXECUTABLE} install pyopenssl
+
+# Checking if mpicc is in /usr/bin
+if [ ! -f /usr/bin/mpicc ] ; then
+    mpicc_path=$(echo $(find /usr -name mpicc) | cut -d' ' -f1)
+    ln -s ${mpicc_path} /usr/bin
+fi
+
+# Checking if mpirun is in /usr/bin
+if [ ! -f /usr/bin/mpirun ] ; then
+    mpirun_path=$(echo $(find /usr -name mpirun) | cut -d' ' -f1)
+    ln -s ${mpirun_path} /usr/bin
+fi
+
+# Checking if libatlas is in /usr/lib
+if [ ! -f /usr/lib/libatlas.so ] ; then
+    ATLAS_PATH=$(find /usr -name libatlas.so)
+    if [ -z "${ATLAS_PATH}" ] ; then
+        ATLAS_PATH=$(find /usr -name libtatlas.so)
+    fi
+
+    ln -s ${ATLAS_PATH} /usr/lib/libatlas.so
+fi
